62fb6e67e62ac9ca89ceb9a08e3f37860a18f415
mnt4 <mnt4@users.noreply.github.com>
2020-07-29 17:26:34 +0300

    changed layout and IPs

--- bot.js
+++ bot.js
@@ -11,14 +11,14 @@ const config = {
 // WesternRP (to be replaced with a DB)
 const server = {
     north: new FiveM.Server('194.87.110.211:30120'),
-    west: new FiveM.Server('194.87.110.211:30121'),
+    west: new FiveM.Server('194.87.110.211:30123'),
     south: new FiveM.Server('194.87.110.211:30122'),
 }
 
 let online = {
-    north: 'Сервер выключен',
-    west: 'Сервер выключен',
-    south: 'Сервер выключен',
+    north: 'Сервер выключен ',
+    west: 'Сервер выключен ',
+    south: 'Сервер выключен ',
 }
 
 
@@ -45,52 +45,82 @@ bot.on('message', message => {
         const onlineMsg = new Discord.MessageEmbed()
         .setTitle('Онлайн серверов:')
         .addField('North', online.north + '/32')
-        .addField('West', online.west + '/32')
+        // .addField('West', online.west + '/32')
         .addField('South', online.south + '/32')
         .setFooter('Made by M4NT4#0001')
         .setThumbnail(message.guild.iconURL())
         .setColor(0x00FF15);
         message.channel.send(onlineMsg);
     };
+
+    if (cmd === 'update') {
+        let lastMsg = message.channel.lastMessage;
+
+        // if (message.author.id != '357930008100077569') return;
+        
+        setInterval(() => {
+            lastMsg.delete();
+
+            server.north.getPlayers().then(data => {
+                online.north = data
+            });
+            // server.west.getPlayers().then(data => {
+            //     online.west = data
+            // });
+            // server.south.getPlayers().then(data => {
+            //     online.south = data
+            // });
+    
+            const onlineMsg = new Discord.MessageEmbed()
+            .setTitle('Онлайн серверов:')
+            .addField('North', online.north + '/32')
+            .addField('West', online.west + '/32')
+            .addField('South', online.south + '/32')
+            .setFooter('Made by M4NT4#0001')
+            .setThumbnail(message.guild.iconURL())
+            .setColor(0x00FF15);
+            lastMsg.edit(onlineMsg);
+        }, 15000);
+    };
 });
 
 // Self-updating message
-bot.on('ready', function () { 
-    setInterval( function() {
-        let msgChannel = 'null'
-        bot.channels.fetch('734455536556179466').then(channel => {
-            msgChannel = channel
-        })
+// bot.on('ready', function () { 
+//     setInterval( function() {
+//         let msgChannel = 'null'
+//         bot.channels.fetch('734455536556179466').then(channel => {
+//             msgChannel = channel
+//         })
 
-        server.north.getPlayers().then(data => {
-            online.north = data
-            console.log(online);
-        });
-        // server.west.getPlayers().then(data => {
-        //     online.west = data
-        //     console.log(online);
-        // });
-        // server.south.getPlayers().then(data => {
-        //     online.south = data
-        //     console.log(online);
-        // });
+//         server.north.getPlayers().then(data => {
+//             online.north = data
+//             console.log(online);
+//         });
+//         // server.west.getPlayers().then(data => {
+//         //     online.west = data
+//         //     console.log(online);
+//         // });
+//         // server.south.getPlayers().then(data => {
+//         //     online.south = data
+//         //     console.log(online);
+//         // });
 
-        const onlineMsg = new Discord.MessageEmbed()
-        .setTitle('Онлайн серверов:')
-        .addField('North', online.north + '/32')
-        .addField('West', online.west + '/32')
-        .addField('South', online.south + '/32')
-        .setFooter('Made by M4NT4#0001')
-        .setThumbnail(bot.guilds.cache.get('703749823169429614').iconURL())
-        .setColor(0x00FF15);
+//         const onlineMsg = new Discord.MessageEmbed()
+//         .setTitle('Онлайн серверов:')
+//         .addField('North', online.north + '/32')
+//         .addField('West', online.west + '/32')
+//         .addField('South', online.south + '/32')
+//         .setFooter('Made by M4NT4#0001')
+//         .setThumbnail(bot.guilds.cache.get('703749823169429614').iconURL())
+//         .setColor(0x00FF15);
         
-        msgChannel.messages.fetch({ limit: 1 }).then(message => {
-            let lastMsg = message.first();
-            lastMsg.delete();
-            msgChannel.send(onlineMsg);
-        });
-    }, 3000);
-});
+//         msgChannel.messages.fetch({ limit: 1 }).then(message => {
+//             let lastMsg = message.first();
+//             lastMsg.delete();
+//             msgChannel.send(onlineMsg);
+//         });
+//     }, 3000);
+// });
 
 bot.on('ready', function() {
     console.log('Bot is on air!')
